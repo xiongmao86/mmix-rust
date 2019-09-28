c1c22d3b6d606cf163064efd774c3c06a307d8fc
xiongmao86 <xiongmao86dev@sina.com>
2019-09-28 21:56:46 +0800

    Setup unit tests.

--- src/instructions.rs
+++ src/instructions.rs
@@ -2,16 +2,20 @@
 
 // Instruction test
 
-fn signed_setup() {
-    // M[1000]M[1001]...M[1007] = 0x0123_4567_89ab_cdef
-    // $2 = 1000
-    // $3 = 2
-}
+#[cfg(test)]
+mod tests {
+    fn signed_setup() {
+        // M[1000]M[1001]...M[1007] = 0x0123_4567_89ab_cdef
+        // $2 = 1000
+        // $3 = 2
+    }
 
-#[test]
-fn test_signed_load() {
-    signed_setup();
+    #[test]
+    fn test_signed_load() {
+        signed_setup();
 
-    // LDB $1, $2, $3
-    // assert $1 = 0x0000_0000_0000_0045
+        // LDB $1, $2, $3
+        // assert $1 = 0x0000_0000_0000_0045
+        assert!(true);
+    }
 }
