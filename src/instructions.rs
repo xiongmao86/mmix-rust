1164f1c1735e201208cacef878e8a24ecab9afc1
xiongmao86 <xiongmao86dev@sina.com>
2019-09-28 22:20:38 +0800

    Setup M for sign load test.

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,8 +4,28 @@
 
 #[cfg(test)]
 mod tests {
+    struct M;
+
+    // M[1000]M[1001]...M[1007] = 0x0123_4567_89ab_cdef
+    impl std::ops::Index<u64> for M {
+        type Output = u8;
+
+        fn index(&self, i: u64) -> &Self::Output {
+            match i {
+                1000 => &0x01u8,
+                1001 => &0x23u8,
+                1002 => &0x45u8,
+                1003 => &0x67u8,
+                1004 => &0x89u8,
+                1005 => &0xabu8,
+                1006 => &0xcdu8,
+                1007 => &0xefu8,
+                _ => &0x00u8,
+            }
+        }
+    }
+
     fn signed_setup() {
-        // M[1000]M[1001]...M[1007] = 0x0123_4567_89ab_cdef
         // $2 = 1000
         // $3 = 2
     }
@@ -16,6 +36,6 @@ mod tests {
 
         // LDB $1, $2, $3
         // assert $1 = 0x0000_0000_0000_0045
-        assert!(true);
+        assert_eq!(M[1002], 0x45);
     }
 }
