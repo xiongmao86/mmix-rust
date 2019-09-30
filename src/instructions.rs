2a124a3d6397ee95d9d186036f0c9103c5ec7c0d
xiongmao86 <xiongmao86dev@sina.com>
2019-10-01 04:17:44 +0800

    Implement signed load wyde.

--- src/instructions.rs
+++ src/instructions.rs
@@ -20,10 +20,20 @@ impl Index<u64> for M {
         }
     }
 }
+
 // Instruction code
 fn signed_load_byte(memory: M, address: u64) -> u64 {
     let b = memory[address];
     let i: i64 = (b as i8).into();
+    i as u64
+}
+
+fn signed_load_wyde(memory: M, address: u64) -> u64 {
+    let k = address - address % 2;
+    let ub: u16 = memory[k].into();
+    let lb: u16 = memory[k + 1].into();
+    let w: u16 = (ub<<8)|lb;
+    let i: i64 = (w as i16).into();
     i as u64
 }
 
@@ -48,5 +58,13 @@ mod tests {
     fn test_signed_load_byte() {
         assert_eq!(signed_load_byte(M, 1002), 0x00_00_00_00_00_00_00_45, "should lead with zero.");
         assert_eq!(signed_load_byte(M, 1004), 0xff_ff_ff_ff_ff_ff_ff_89, "should lead with one.");
+    }
+
+    #[test]
+    fn test_signed_load_wyde() {
+        assert_eq!(signed_load_wyde(M, 1002), 0x00_00_00_00_00_00_45_67, "should lead with zero.");
+        assert_eq!(signed_load_wyde(M, 1003), 0x00_00_00_00_00_00_45_67, "should be the same with 1002.");
+        assert_eq!(signed_load_wyde(M, 1004), 0xff_ff_ff_ff_ff_ff_89_ab, "should lead with one.");
+        assert_eq!(signed_load_wyde(M, 1005), 0xff_ff_ff_ff_ff_ff_89_ab, "should be the same with 1004.");
     }
 }
