5ea69f1df8dec9def085a2df268b3bd2f3345c12
xiongmao86 <xiongmao86dev@sina.com>
2019-10-01 15:22:43 +0800

    Finish signed load.

--- src/instructions.rs
+++ src/instructions.rs
@@ -59,18 +59,25 @@ impl M {
 
 // Instruction code
 fn signed_load_byte(memory: M, address: u64) -> u64 {
-    let b = memory[address];
+    let b = memory.byte(address);
     let i: i64 = (b as i8).into();
     i as u64
 }
 
 fn signed_load_wyde(memory: M, address: u64) -> u64 {
-    let k = address - address % 2;
-    let ub: u16 = memory[k].into();
-    let lb: u16 = memory[k + 1].into();
-    let w: u16 = (ub << 8) | lb;
+    let w = memory.wyde(address);
     let i: i64 = (w as i16).into();
     i as u64
+}
+
+fn signed_load_tetra(memory: M, address: u64) -> u64 {
+    let t = memory.tetra(address);
+    let i: i64 = (t as i32).into();
+    i as u64
+}
+
+fn signed_load_octa(memory: M, address: u64) -> u64 {
+    memory.octa(address)
 }
 
 #[cfg(test)]
@@ -158,6 +165,26 @@ mod tests {
             signed_load_wyde(M, 1005),
             0xff_ff_ff_ff_ff_ff_89_ab,
             "should be the same with 1004."
+        );
+    }
+
+    #[test]
+    fn test_signed_load_tetra() {
+        assert_eq!(
+            signed_load_tetra(M, 1001),
+            0x00_00_00_00_01_23_45_67
+        );
+        assert_eq!(
+            signed_load_tetra(M, 1005),
+            0xff_ff_ff_ff_89_ab_cd_ef
+        );
+    }
+
+    #[test]
+    fn test_signed_load_octa() {
+        assert_eq!(
+            signed_load_octa(M, 1006),
+            0x01_23_45_67_89_ab_cd_ef
         );
     }
 }
