1f9332ba417d79932c60db76ded3857a1f58b321
xiongmao86 <xiongmao86dev@sina.com>
2019-10-15 20:43:40 +0800

    Setup machine test enviroment.

--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -2,23 +2,42 @@ mod memory;
 use memory::Memory;
 
 #[allow(dead_code)]
-struct Machine<'a> {
-    memory: &'a dyn Memory,
+struct Machine<T: Memory> {
+    memory: T,
     general_register: [u64; 256],
     special_register: [u64; 32]
 }
 
+impl<T> Machine<T> where T: Memory {
+    fn new(memory: T) -> Machine<T> {
+        Machine {
+            memory,
+            general_register: [0; 256],
+            special_register: [0; 32],
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    fn signed_setup() {
+    use super::memory::HashMemory;
+    use super::*;
+
+    fn machine_for_tests() -> Machine<HashMemory> {
         // $2 = 1000
         // $3 = 2
+        let mut hm = HashMemory::new();
+        hm.store_octa(1000, 0x01_23_45_67_89_ab_cd_efu64);
+        let mut m = Machine::new(hm);
+        m.general_register[1] = 1000;
+        m.general_register[2] = 2;
+        m
     }
 
     //LDB
     #[test]
     fn test_ldb() {
-        signed_setup();
+        let m = machine_for_tests();
 
         // LDB $1, $2, $3
         // assert $1 = 0x0000_0000_0000_0045
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(associated_type_bounds)]
+
 mod machine;
 
 fn main() {
